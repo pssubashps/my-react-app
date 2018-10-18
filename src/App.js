fbd866eaa65c6859a4ad8c42c82dae7237bacb29
Subash <pssubashps@gmail.com>
2018-10-18 09:52:35 +0530

    Remove unsed reference

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout'
-import Portfolio from './containers/Portfolio/Portfolio';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-       <Layout>
-          
+       <Layout>          
        </Layout>
      
     );
